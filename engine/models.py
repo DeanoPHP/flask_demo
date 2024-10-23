0337d4de23bd5af33b2e3e8cd0011401d14a54a4
DeanoLark <deanolark@gmail.com>
2024-10-23 23:32:45 +0100

    created register and added User model

--- engine/models.py
+++ engine/models.py
@@ -1,4 +1,5 @@
 from . import db
+from flask_login import UserMixin
 
 class Task(db.Model):
     __tablename__ = "tasks"
@@ -8,3 +9,16 @@ class Task(db.Model):
 
     def __repr__(self) -> str:
         return f"Task set with the id {self.id}"
+    
+
+class User(db.Model, UserMixin):
+    __tablename__ = "Users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.String(60), unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"You have successfully registered with username {self.username}"
--- engine/routes.py
+++ engine/routes.py
@@ -1,6 +1,20 @@
 from flask import render_template, redirect, request, url_for
 from . import app, db
-from .models import Task
+from .models import Task, User
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, login_required, UserMixin, logout_user, current_user
+
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'auth'
+
+app.config["SECRET_KEY"] = "jr134244a"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+bcrypt = Bcrypt(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,22 +28,32 @@ def home():
     todos = Task.query.all()
     return render_template("index.html", todos=todos)
 
-# here i want a route that handles show login
-# form or register form when clicked
-
 
 @app.route("/auth", methods=["GET", "POST"])
 def auth():
-    form_type = request.args.get("form", "login")
+    form_type = request.args.get("form_type")
     if request.method == "POST":
-
         if form_type == "login":
             username = request.form.get("username")
             password = request.form.get("password")
-            return redirect(url_for("dashboard"))
+            return redirect(url_for("home"))
         if form_type == "register":
+            name = request.form.get("name")
+            email = request.form.get("email")
             username = request.form.get("username")
             password = request.form.get("password")
-            return redirect(url_for("login"))
-        
+            hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+            
+            new_user = User(name=name, email=email, username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for("dashboard"))
+
+    form_type = request.args.get("form", "login")    
     return render_template("auth.html", form_type = form_type)
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template('dashboard.html', username=current_user.username)
